019b1ec6553e0c424601bed5b4c8df73a9dd9b5d
Flu3nt <gnedyalkov94@gmail.com>
2022-11-18 13:02:01 +0200

    Add links to index page

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,7 +1,5 @@
 import Layout from "../components/layout";
-import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -24,16 +22,12 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingX}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateStrinng={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
-      </article>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
     </Layout>
   );
 }
